a848b0e196e875e350817710340ae4b295cd04bd
Unknown <jellalfan21@gmail.com>
2018-10-04 10:34:23 +0530

    Added constraint layout for responsive(multiple device) display

--- app/src/main/java/com/example/ishakadam/restrauntreservation/MainActivity.java
+++ app/src/main/java/com/example/ishakadam/restrauntreservation/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.ishakadam.restrauntreservation;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt=(TextView)findViewById(R.id.tafel);
+
+    }
+    public void onClick(View v){
+            txt.setTextColor(Color.parseColor("#00ff33"));
+        Toast.makeText(this,"Welcome to the restaurant reservation app",Toast.LENGTH_LONG).show();
     }
 }
